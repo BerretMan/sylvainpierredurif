630798e0dbdd48a2c7279dbcb8a3bd87391768c2
UltimeMan <79540579+UltimeMan@users.noreply.github.com>
2021-11-21 18:42:35 +0100

    new comit
    this is a commit

--- bot.py
+++ bot.py
@@ -4,12 +4,9 @@ from random import*
 
 bot = commands.Bot(command_prefix = "!", description = "Je suis le Bot et L'esclave de @LucasS#2717",help_command=None)
 
-
-
 @bot.event
 async def on_ready():
     print("Ready ")
-
 
 
 @bot.command()
@@ -18,16 +15,38 @@ async def speak(ctx, arg):
     await ctx.delete(arg)
     
     
-insult = ["tu as réussis l'exploit d'être plus cringe qu'évan", "tu as les rotations d'un bronze","tu es cringe", "ratio", "tu es un inter professionnelle"]
+insult = ["tu as réussis l'exploit d'être plus cringe qu'evan", "tu as les rotations d'un bronze","f'in fréro, tu es cringe", "ratio", "fin frerot allume l'écran"]
 @bot.command()
-async def flame(ctx, member : discord.Member, *, reason=None,a=None) :
+async def flame(ctx, member : discord.Member,a=None) :
     if a == None:
-        a = randint(0,2)
+        a = randint(0,5)
     await ctx.send(f" Salut {member.mention} {insult[a]}")
     
+
+lovelist = ["Tu travaillerais pas chez AirFrance? Parceque j'ai un avion de chasse en face de moi","Tu serais pas un dm de maths? Parceque j'ai vraiment envie de te terminer à 4h du mat sur une table"]
+@bot.command()
+async def seduce(ctx, member : discord.Member,a=None) :
+    if a == None:
+        a = randint(0,1)
+    await ctx.send(f" Salut {member.mention} {lovelist[a]}")
+    
+
 @bot.command()
 async def help(ctx) :
     embed = discord.Embed(title="Je suis Sylvain Pierre Durif, grand monarque christ cosmique le messi, l'homme vert, messager de Dieu, et crée par @LucasS") #,color=Hex code
-    embed.add_field(name="!flame (tag) (insulte optionnelle)", value="Insulte violemment tes mates avec la force du messi")   
+    embed.add_field(name="!flame (tag) (insulte optionnelle)", value="Insulte violemment tes mates avec la force du messi")  
+    embed.add_field(name="!seduce (tag) (insulte optionnelle)", value="Utilise la puissance du messi pour conquérir ta dulciné (ne fonctionne pas si vous jouer à lol)")    
     await ctx.send(embed=embed)
-bot.run("ODkwMzAzMDA0OTM3NDUzNjA4.YUt1SQ.mcjcu0e2EdUA60rNKO_jJbOX7kk")
+
+
+
+
+
+
+
+
+
+
+
+
+bot.run("ODkwMzAzMDA0OTM3NDUzNjA4.YUt1SQ.2WYr-Ok6bFYve2C8lPmJYyLHCew")
